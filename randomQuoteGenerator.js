9dc842e7f41dfdb25b038911b963f96770c492bb
LuisGonzaga <luis.caldas.costa@gmail.com>
2016-07-24 17:14:01 +0100

    Update randomQuoteGenerator.js

--- randomQuoteGenerator.js
+++ randomQuoteGenerator.js
@@ -11,7 +11,8 @@ var http = require('http');
 var server = http.createServer(handleRequest);
 
 function handleRequest(request, response){
-    //response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    response.addHeader('Content-Type', 'text/html; charset=UTF-8');
+    response.addHeader("Access-Control-Allow-Origin", "*");
     response.end(rndQuote());
 }
 
