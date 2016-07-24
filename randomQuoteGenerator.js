a7bfe635e84c244f341152c1be330d7e0f40416f
LuisGonzaga <luis.caldas.costa@gmail.com>
2016-07-24 17:02:37 +0100

    Update randomQuoteGenerator.js

--- randomQuoteGenerator.js
+++ randomQuoteGenerator.js
@@ -8,12 +8,11 @@ var port= process.env.PORT;
 
 var http = require('http');
 
+var server = http.createServer(handleRequest);
 /* Because of the "XMLHttpRequest cannot load " error this was commented, 
 the error seems to be raised because ot attempting to set the Header in a GET request 
 
 var app = express();
-
-var server = http.createServer(handleRequest);
 
 function handleRequest(request, response){
     response.setHeader('Content-Type', 'text/html; charset=UTF-8');
